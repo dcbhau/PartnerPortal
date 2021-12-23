b5906dbf510f6a684a09868d32b8ee52ac76547d
DC <dhruv.chauhan2711@gmail.com>
2021-12-23 13:22:32 +0530

    gh-pages

--- js/nav.js
+++ js/nav.js
@@ -1,16 +1,17 @@
 window.onscroll = function () {
-    scrollFunction();
-  };
-  function scrollFunction() {
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-      document.getElementById("logo").style.width = "130px";
-      document.getElementById("logo").style.height = "52px";
-      document.getElementById("scroll").classList.add("shadow");
-    } else {
-      document.getElementById("logo").style.width = "175px";
-      document.getElementById("logo").style.height = "70px";
-      document.getElementById("scroll").classList.remove("shadow");
-    }
+  scrollFunction();
+};
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    document.getElementById("logo").style.width = "130px";
+    document.getElementById("logo").style.height = "52px";
+    document.getElementById("scroll").classList.add("shadow");
+  } else {
+    document.getElementById("logo").style.width = "175px";
+    document.getElementById("logo").style.height = "70px";
+    document.getElementById("scroll").classList.remove("shadow");
   }
- 
-  
+}
